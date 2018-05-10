424eaa9adb39698b8648cf8a51b112ce80cca3c4
yu <ybw2016v@126.com>
2018-05-10 21:32:27 +0800

    update

--- fdtdcal.c
+++ fdtdcal.c
@@ -1,6 +1,8 @@
 #include "fdtdcal.h"
 int initecal(double ex[],double ey[],double ez[],int strides[],int shapes[])
 {
+// 对电场矩阵进行初始化。
+// ex,ey,ez为电场矩阵，strides为矩阵行列标识，shape为矩阵形状向量
     I=shapes[0];
     J=shapes[1];
     K=shapes[2];
@@ -15,6 +17,7 @@ int initecal(double ex[],double ey[],double ez[],int strides[],int shapes[])
 }
 int initbcal(double bx[],double by[],double bz[],int strides[],int shapes[])
 {
+    // 对磁场矩阵进行初始化
     bx1=bx;
     by1=by;
     bz1=bz;
@@ -23,6 +26,7 @@ int initbcal(double bx[],double by[],double bz[],int strides[],int shapes[])
 }
 int inite2cal(double ex[],double ey[],double ez[],int strides[],int shapes[])
 {
+    // 对电场矩阵进行初始化。
     ex2=ex;
     ey2=ey;
     ez2=ez;
@@ -31,6 +35,7 @@ int inite2cal(double ex[],double ey[],double ez[],int strides[],int shapes[])
 }
 int initb2cal(double bx[],double by[],double bz[],int strides[],int shapes[])
 {
+// 对磁场矩阵进行初始化
     bx2=bx;
     by2=by;
     bz2=bz;
@@ -39,7 +44,9 @@ int initb2cal(double bx[],double by[],double bz[],int strides[],int shapes[])
 }
 int cal(int n)
 {
-    double * tem;
+// 计算核心函数
+// n为计算次数
+    double * tem;//暂存指针。
     
     for (int ii = 0; ii < n; ii++)
     {
@@ -53,6 +60,7 @@ int cal(int n)
                 #pragma omp parallel for
                 for(int k = 2; k < K-1; k++)
                 {
+                    // 迭代核心
                     ex2[S0*i+S1*j+S2*k]=1*ex1[S0*i+S1*j+S2*k]+1*(1/det)*((bz1[S0*i+S1*j+S2*k]-bz1[S0*i+S1*(j-1)+S2*k])-by1[S0*i+S1*j+S2*k]+by1[S0*i+S1*j+S2*(k-1)]);
                     ey2[S0*i+S1*j+S2*k]=1*ey1[S0*i+S1*j+S2*k]+1*(1/det)*((bx1[S0*i+S1*j+S2*k]-bx1[S0*i+S1*j+S2*(k-1)])-bz1[S0*i+S1*j+S2*k]+bz1[S0*(i-1)+S1*j+S2*(k)]);
                     ez2[S0*i+S1*j+S2*k]=1*ez1[S0*i+S1*j+S2*k]+1*(1/det)*((by1[S0*i+S1*j+S2*k]-by1[S0*(i-1)+S1*j+S2*(k)])-bx1[S0*i+S1*j+S2*k]+by1[S0*(i)+S1*(j-1)+S2*(k)]);
@@ -65,6 +73,7 @@ int cal(int n)
             
         }
     // printf("%s \n","66");
+    // 指针交换。
     tem=ex1;
     ex1=ex2;
     ex2=tem;
@@ -94,7 +103,16 @@ int cal(int n)
 }
 int intarry(double adet)
 {
+    // 初始化网格细度。
     det=adet;
     return 0;
+}
+int initc(double *cca,double * ccb,double * ccp,double * ccq)
+{
+    // 对电磁介质进行初始化；
+    ca=cca;
+    cb=ccb;
+    cp=ccp;
+    cq=ccq;
 }
 
--- fdtdcal.h
+++ fdtdcal.h
@@ -1,23 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+// 函数声明
 int initecal(double ex[],double ey[],double ez[],int strides[],int shapes[]);
 int initbcal(double bx[],double by[],double bz[],int strides[],int shapes[]);
 int inite2cal(double ex[],double ey[],double ez[],int strides[],int shapes[]);
 int initb2cal(double bx[],double by[],double bz[],int strides[],int shapes[]);
 int intarry(double adet);
 int cal(int n);
-double det=2;
-int S0=0;
-int S1=0;
-int S2=0;
-double * ex1;
+int initc(double *cca,double * ccb,double * ccp,double * ccq);
+// 全局变量
+double det=2;//网格细度
+int S0=0;//矩阵行列标识
+int S1=0;//矩阵行列标识
+int S2=0;//矩阵行列标识
+int I,J,K;//矩阵长度极限
+double * ex1;//步进电磁场矩阵
 double * ey1;
 double * ez1;
 double * bx1;
 double * by1;
 double * bz1;
-int I,J,K;
+
 int flog=0;
 double * ex2;
 double * ey2;
@@ -25,3 +29,7 @@ double * ez2;
 double * bx2;
 double * by2;
 double * bz2;
+double * ca;//介质参数
+double * cb;
+double * cp;
+double * cq;
